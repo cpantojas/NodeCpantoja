501c51411db63c476737a71d16895fc38ad5ce74
Cristian <cpsalazar@gmail.com>
2021-07-20 22:21:56 -0400

    se agrega primera version desafio1.js

--- desafio1.js
+++ desafio1.js
@@ -1,5 +1,9 @@
 let texto="Node. js es un entorno de tiempo de ejecución de JavaScript (de ahí su terminación en . js haciendo alusión al lenguaje JavaScript). Este entorno de tiempo de ejecución en tiempo real incluye todo lo que se necesita para ejecutar un programa escrito en JavaScript."
-
+//limpiando cadena
+let texto=texto.replace('.','');
+let texto=texto.replace('(','');
+let texto=texto.replace(')','');
+console.log(texto);
 //contar cantidad de veces que esta cada palabra en el texto, omitir puntos comas y parentesis
 //node=2
 //js=1
